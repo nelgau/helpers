c3bbeb0f82d3924f2d4a6b942ffa12309bc64e42
Nelson Gauthier <nelson.gauthier@gmail.com>
2022-10-01 22:59:41 +0000

    Bootstrap pyenv with a recent python version

--- lambda_labs/user_create.sh
+++ lambda_labs/user_create.sh
@@ -15,6 +15,10 @@ if [ -d "$userhome" ]; then
     exit 1
 fi
 
+##
+# Create User
+#
+
 # Create the user and home directory
 useradd -m -d "$userhome" -s "$usershell" "$username"
 
@@ -27,3 +31,33 @@ chmod 600 "$userhome/.ssh/authorized_keys"
 
 # Allow passwordless sudo
 echo "$username ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$username"
+
+##
+# Install Pyenv
+#
+
+pythonver=3.10.6
+
+# Run the bootstrap script
+su -l "$username" -c "curl https://pyenv.run | bash"
+
+# Append shell configuration to .bashrc
+cat >> "$userhome/.bashrc" <<"END"
+
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+END
+
+# Append shell configuration to .profile
+cat >> "$userhome/.profile" <<"END"
+
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+END
+
+# Build recent python and make it the global interpreter
+su -l "$username" -c "pyenv install $pythonver"
+su -l "$username" -c "pyenv global $pythonver"
