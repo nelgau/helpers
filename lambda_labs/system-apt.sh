d14f859b4ecd711e4b34019178213b05e8c6e3dd
Nelson Gauthier <nelson.gauthier@gmail.com>
2022-10-01 20:11:26 -0700

    Assume non-interactive yes during apt-get install

--- lambda_labs/system-apt.sh
+++ lambda_labs/system-apt.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 apt-get update
 
-apt-get install make build-essential libssl-dev zlib1g-dev \
+apt-get --yes install make build-essential libssl-dev zlib1g-dev \
     libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
     libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
     libffi-dev liblzma-dev
