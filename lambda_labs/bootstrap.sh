c49c29dfe63fe12c93f46194ce682a5f3ba6edf1
Nelson Gauthier <nelson.gauthier@gmail.com>
2022-10-01 19:41:43 -0700

    Add bootstrapping script that can be piped into the shell

---
+++ lambda_labs/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -euxo pipefail
+
+git config --global user.name "Nelson Gauthier"
+git config --global user.email "nelson.gauthier@gmail.com"
+
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+rm -rf helpers
+git clone git@github.com:nelgau/helpers.git
+
+sudo helpers/lambda_labs/user_create.sh
